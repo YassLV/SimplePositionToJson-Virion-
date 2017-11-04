42aaa26f8dba70226e2776bd6874068cefd20268
MineBuilderFR <minebuilderfr11@gmail.com>
2017-11-05 00:46:08 +0100

    Add Documentation

--- src/MineBuilderFR/SimplePositionToJson/BaseINT.php
+++ src/MineBuilderFR/SimplePositionToJson/BaseINT.php
@@ -6,8 +6,14 @@ namespace MineBuilderFR\SimplePositionToJson;
 interface BaseINT
 {
 
+    /**
+     * @return array
+     */
     public function AllPosition() : array;
 
+    /**
+     * @return string
+     */
     public function getJsonName() : string;
 
 }
